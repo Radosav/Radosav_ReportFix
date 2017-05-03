5d5a5fab1a222bd8867f4398f98af7dc75fe5223
Radosav Brajic <rasabrajic@gmail.com>
2017-05-03 15:15:00 +0200

    Init

---
+++ app/code/Radosav/Reportfix/Model/Magento/Reports/ResourceModel/Quote/Item/Collection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Radosav\Reportfix\Model\Magento\Reports\ResourceModel\Quote\Item;
+
+
+class Collection extends \Magento\Reports\Model\ResourceModel\Quote\Item\Collection
+{
+    protected function _afterLoad()
+    {
+        parent::_afterLoad();
+        $items = $this->getItems();
+        $productIds = [];
+        foreach ($items as $item) {
+            $productIds[] = $item->getProductId();
+        }
+        $productData = $this->getProductData($productIds);
+        $orderData = $this->getOrdersData($productIds);
+        $existing = $this->productResource->getAllIds();
+        foreach ($items as $item) {
+            if(in_array($item->getProductId(),$existing)){
+                continue;
+            }
+            $item->setId($item->getProductId());
+            $item->setPrice($productData[$item->getProductId()]['price'] * $item->getBaseToGlobalRate());
+            $item->setName($productData[$item->getProductId()]['name']);
+            $item->setOrders(0);
+            if (isset($orderData[$item->getProductId()])) {
+                $item->setOrders($orderData[$item->getProductId()]['orders']);
+            }
+        }
+
+        return $this;
+    }
+}
+	
+	
